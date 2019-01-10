81c577273990bd4b4efc5ff39bcafbdf009c1403
Camille SCHWARZ <camille.schwarz9@etu.univ-lorraine.fr>
2019-01-10 14:04:38 +0100

    Récupération du catalogue des musiques en JSON v2

--- catalogue.php
+++ catalogue.php
@@ -1,5 +1,7 @@
 <?php
-
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization');
 $conf = parse_ini_file("conf/conf.ini");
 $dbh = new PDO('mysql:host=localhost;dbname=rimet2u', $conf["id"], $conf["mdp"],array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 $tabPistes = [];
@@ -8,7 +10,6 @@ $compteurGenre = 0;
 $compteurArtiste = 0;
 $compteurAlbum = 0;
 foreach ($dbh->query('SELECT * from piste ORDER BY idPiste') as $row) {
-    //$piste = $dbh->query('SELECT * from piste where idPiste=' . $row['idPiste'])->fetch();
     $tabPistes[$compteur]['idPiste'] = $row['idPiste'];
     $tabPistes[$compteur]['nomPiste'] = $row['nomPiste'];
     $tabPistes[$compteur]['annéePiste'] = $row['annéePiste'];
